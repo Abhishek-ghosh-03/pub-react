47dc341d618bac704ea8e3a43dd4cca71ef71ad4
Abhishek-Ghosh <144532967+Abhishek-ghosh-03@users.noreply.github.com>
2023-10-05 00:38:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Hero from './components/Hero'
 import HeadlineCards from './components/HeadlineCards'
 import Food from './components/Food'
 import Category from './components/Category'
-
 function App() {
   return (
     <div>
