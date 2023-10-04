46e8555bd76a6a3666f148ed02fb789306c40cdb
Abhishek-Ghosh <144532967+Abhishek-ghosh-03@users.noreply.github.com>
2023-10-05 00:35:16 +0530

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -4,7 +4,7 @@ export const data = [
       name: 'Double Cheeseburger',
       category: 'burger',
       image:
-        'https://images.unsplash.com/photo-1607013251379-e6eecfffe234?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60',
+    'https://images.unsplash.com/photo-1607013251379-e6eecfffe234?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60',
       price: '$$$$',
     },
     {
